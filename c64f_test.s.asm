825a4cac411a5e73117bc4713985e5f2115ee3d2
Negative Charge <negativecharge6502@gmail.com>
2023-11-01 05:30:43 +0000

    Decompressor size calc

--- c64f_test.s.asm
+++ c64f_test.s.asm
@@ -50,11 +50,13 @@ SAVE "C64F", start, end, entry_point
 PRINT "------------------------"
 PRINT "   c64f Decompressor    "
 PRINT "------------------------"
-PRINT "CODE size      = ", ~end-start
+PRINT "CODE size         = ", ~end-start
+PRINT "DECOMPRESSOR SIZE = ", entry_point-start, "bytes"
+PRINT "ZERO PAGE SIZE    = ", zp_end-zp_start, "bytes"
 PRINT "------------------------"
-PRINT "LOAD ADDR      = ", ~start
-PRINT "HIGH WATERMARK = ", ~P%
-PRINT "RAM BYTES FREE = ", ~LOAD_ADDR-P%
+PRINT "LOAD ADDR         = ", ~start
+PRINT "HIGH WATERMARK    = ", ~P%
+PRINT "RAM BYTES FREE    = ", ~LOAD_ADDR-P%
 PRINT "------------------------"
 
 PUTBASIC "loader.bas","LOADER"
